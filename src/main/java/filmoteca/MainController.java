afa29a69705e39c8af026698b8b3dbcc650ec1f7
unknown <aluno@h3-05.faeterj-rio.edu.br>
2015-06-03 22:03:12 -0300

    Ignora a pasta target no git

---
+++ src/main/java/filmoteca/MainController.java
@@ -0,0 +1,17 @@
+package filmoteca;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class MainController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Alo FilmotecaWebBruno!";
+    }
+}
