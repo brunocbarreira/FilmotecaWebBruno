8f2da42b75919efd4df97310b71ce9f92364a1fa
unknown <aluno@h3-05.faeterj-rio.edu.br>
2015-06-03 21:16:52 -0300

    Inicializacao do projeto

---
+++ src/main/java/filmoteca/FilmotecaApplication.java
@@ -0,0 +1,12 @@
+package filmoteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmotecaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmotecaApplication.class, args);
+    }
+}
